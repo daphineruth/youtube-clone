08354f92f9bd7e1ccdeeaf529a84a75c5241c05c
sheiswiz <daphneruth@304gmail.com>
2022-03-03 00:48:13 +0300

    updated

--- mousemove  &  mouseevents/script.js
+++
@@ -1,7 +0,0 @@
-function setNewImage(){
-    document.getElementById("img").src = "images/web2.jpeg";
-}
-
-function setOldImage () {
-    document.getElementById("img").src = "images/web1.jpeg";;
-}
--- selection in dropdown list/script.js
+++
@@ -1,6 +0,0 @@
-function function1() {
-
-    var select = document.getElementById ("selectbox");
-    alert(select.options[select.selectedIndex].value
-        )
-}
