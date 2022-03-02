8683541e5f24e762ebe681f4d78a3795ca6f3c46
sheiswiz <daphneruth@304gmail.com>
2022-03-02 22:46:36 +0300

    updated

---
+++ mousemove  &  mouseevents/script.js
@@ -0,0 +1,7 @@
+function setNewImage(){
+    document.getElementById("img").src = "images/web2.jpeg";
+}
+
+function setOldImage () {
+    document.getElementById("img").src = "images/web1.jpeg";;
+}
